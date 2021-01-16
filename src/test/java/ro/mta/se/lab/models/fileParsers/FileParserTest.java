300e484aed77467a0ff1c05fd535391a7e964c34
bianca <bianca.basescu@yahoo.com>
2021-01-16 22:51:57 +0200

    timeout test fileParser
    Signed-off-by: bianca <bianca.basescu@yahoo.com>

--- src/test/java/ro/mta/se/lab/models/fileParsers/FileParserTest.java
+++ src/test/java/ro/mta/se/lab/models/fileParsers/FileParserTest.java
@@ -1,10 +1,15 @@
 package ro.mta.se.lab.models.fileParsers;
 
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileParserTest {
 
     @org.junit.jupiter.api.Test
+    @Timeout(value = 600, unit = TimeUnit.MILLISECONDS)
     void getSpecificCityId() throws Exception {
         FileParser fileParser = new FileParser();
         fileParser.readConfFile("files/inputRO.txt");
